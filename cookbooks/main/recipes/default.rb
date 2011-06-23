c95fd62f8b5f6c56a3a53fbfb524317431521553
Jim Neath <jimneath@googlemail.com>
2011-06-23 15:49:55 +0100

    memcached cluster recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,3 +1,5 @@
+require_recipe "memcached_cluster"
+
 #execute "testing" do
 #  command %Q{
 #    echo "i ran at #{Time.now}" >> /root/cheftime
--- cookbooks/memcached_cluster/recipes/default.rb
+++ cookbooks/memcached_cluster/recipes/default.rb
@@ -1,16 +1,11 @@
-#
-# Cookbook Name:: memcached_util
-# Recipe:: default
-#
-
 # settings
-appname = "app_name"
+appname = "content_length_bug"
 
-# find memcached util hostname
-memcached_hosts = @node['utility_instances'].select{|i| i['name'] == util_name}[0]['hostname']
+# find memcached hosts
+memcached_hosts = node[:utility_instances].select{|i| i[:name][/^memcache/i]}.map{|i| i[:hostname]}
 
 # update memcached.yml on instances
-if ['app_master', 'app', 'util'].include?(node[:instance_role])
+if ['app_master', 'app', 'util', 'solo'].include?(node[:instance_role])
   run_for_app(appname) do |app_name, data|
     template "/data/#{app_name}/shared/config/memcached.yml" do
       owner node[:owner_name]
@@ -19,7 +14,7 @@ if ['app_master', 'app', 'util'].include?(node[:instance_role])
       backup 0
       source "memcached.yml.erb"
       variables({
-        :memcached_host => memcached_host,
+        :hosts => memcached_hosts,
         :app_name => app_name,
         :mem_limit => 64
       })
@@ -28,23 +23,8 @@ if ['app_master', 'app', 'util'].include?(node[:instance_role])
 end
 
 # update util instance
-if node[:instance_role] == 'util' && node[:name] == util_name
+if node[:instance_role] == 'util' && node[:name][/^memcache/i]
   run_for_app(appname) do |app_name, data|
-    # report
-    ey_cloud_report "Memcached" do
-      message "upgrading memcached"
-    end
-
-    # upgrade sphinx
-    enable_package "net-misc/memcached" do
-      version "1.4.5"
-    end
-    
-    package "net-misc/memcached" do
-      version "1.4.5"
-      action :upgrade
-    end
-    
     # monit
     remote_file "/etc/monit.d/memcached.monitrc" do
       owner "root"
