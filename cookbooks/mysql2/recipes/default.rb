5cd2763c3faefc4c922810b09f3822e3191c19e3
Jim Neath <jimneath@googlemail.com>
2011-08-09 11:49:16 +0100

    updated mysql2 recipep to be aware of apps

--- cookbooks/mysql2/recipes/default.rb
+++ cookbooks/mysql2/recipes/default.rb
@@ -1,6 +1,8 @@
 # fix database.yml for mysql2
 if ['app_master', 'app', 'util', 'solo'].include?(node[:instance_role])
-  execute "fixing database.yml" do
-    command "sed -i s/mysql$/mysql2/ /data/shoplove/shared/config/database.yml"
+  node[:applications].each do |app_name, data| 
+    execute "fixing database.yml" do
+      command "sed -i s/mysql$/mysql2/ /data/#{app_name}/shared/config/database.yml"
+    end
   end
 end
