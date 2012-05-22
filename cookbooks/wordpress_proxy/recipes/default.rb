df90e56825bc4807734a27baff48caba796f52d0
Jim Neath <jimneath@googlemail.com>
2012-05-22 10:59:44 +0100

    Added sample wordpress proxy recipe

---
+++ cookbooks/wordpress_proxy/recipes/default.rb
@@ -0,0 +1,15 @@
+# update this
+app_name = 'willh'
+blog_url = 'http://blog.com/'
+
+# write custom.conf
+if ['app_master', 'app', 'solo'].include?(node[:instance_role])
+  template "/data/nginx/servers/#{app_name}/custom.conf" do
+    source 'custom.conf.erb'
+    owner 'deploy'
+    group 'deploy'
+    mode 0644
+    backup false
+    attributes :blog_url => blog_url
+  end
+end
