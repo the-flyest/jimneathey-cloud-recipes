ed0b9e13fba5b65bd87c5ff7d45dfbcda70fa14f
Jim Neath <jimneath@googlemail.com>
2012-05-31 09:04:12 +0100

    Updated wordpress proxy recipe

--- cookbooks/wordpress_proxy/recipes/default.rb
+++ cookbooks/wordpress_proxy/recipes/default.rb
@@ -1,5 +1,5 @@
 # update this
-app_name = 'willh'
+app_name = 'appname'
 blog_url = 'http://blog.com/'
 
 # write custom.conf
@@ -10,7 +10,9 @@ if ['app_master', 'app', 'solo'].include?(node[:instance_role])
     group 'deploy'
     mode 0644
     backup false
-    attributes :blog_url => blog_url
+    variables({
+      :blog_url => blog_url
+    }) 
   end
 end
 
